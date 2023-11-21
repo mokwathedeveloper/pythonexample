c4509a9e0563c1af39e1e80d406f46e222adc721
mokwathedeveloper <moffatmokwa12@gmail.com>
2023-11-21 14:04:53 +0300

    this my example workspace

---
+++ study.py
@@ -0,0 +1,46 @@
+dog_name = "Lucy"
+print(f"Say hello to my dog {dog_name}")
+
+price_1 =3
+price_2 = 2.5
+ 
+print(f"Item 1 cost ${price_1:.2f}")
+print(f"Item 2 cost ${price_2:.2f}")
+
+list_abc = ["a" , "b" ,"c"]
+print(list_abc)
+print(list_abc[0])
+print(list_abc[1])
+print(list_abc[2])
+
+len([1,2,3])
+print(len([1,3,400, 7]))
+
+sorted([5, 100, 234, 7, 2])
+print(sorted([5, 100, 234, 7, 2]))
+
+list_123 = [1,2,3]
+print(list_123.pop())
+
+list_123.remove(1)
+print(list_123)
+
+#tuples
+
+(1,2,3)
+tuple_123 = (1,2,3)
+print(tuple_123)
+
+#Sets and Dicts
+# Sets
+my_set = ([3,2,3, "a", "b" "a"])  # Create a set
+print(my_set) 
+
+#dicts
+my_dict ={"key1": 1, "key2": 2}
+print("key3")
+print(my_dict.get("key3"))
+
+dict(x=1, y=2)
+print(dict(x=1, y=2))
+
